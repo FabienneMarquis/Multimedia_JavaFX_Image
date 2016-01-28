1ca6ae09868e12d49eae878b6bcea52f5ba4a1ed
FabienneMarquis <fabienne.marquis@gmail.com>
2016-01-28 13:21:44 -0500

    yay! everything work

--- src/controller/ControllerFXML.java
+++ src/controller/ControllerFXML.java
@@ -1,14 +1,23 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Slider;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.text.Text;
 
-public class ControllerFXML {
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class ControllerFXML implements Initializable{
 
     @FXML
     private Text textAngle;
@@ -23,36 +32,120 @@ public class ControllerFXML {
     private Slider sliderTransparence;
 
     @FXML
-    private Spinner<?> spinnerHigth;
+    private Spinner<Integer> spinnerHigth;
 
     @FXML
-    private Spinner<?> spinnerWidth;
+    private Spinner<Integer> spinnerWidth;
 
     @FXML
-    private Spinner<?> spinnerIteration;
+    private Spinner<Integer> spinnerIteration;
 
     @FXML
-    private ImageView imageView;
-
-
+    private ImageView imagetoModified;
 
     @FXML
-    void rotateImageView(ActionEvent event) {
+    private  CheckBox  checkBoxblurEffect;
+
+    private Image imageBase;
+
+    @FXML
+    void setAngleImageView(MouseEvent event) {
+        textAngle.setText(Double.toString(sliderAngle.getValue()));
+        imagetoModified.setRotate(sliderAngle.getValue());
 
     }
 
     @FXML
-    void setEffetOnImage(ActionEvent event) {
+     void setTransparencyImageview(MouseEvent event) {
+        textTransparence.setText(Double.toString(sliderTransparence.getValue()));
+        imagetoModified.setOpacity(sliderTransparence.getValue()/100);
+    }
+
+    @FXML
+    void blurOn (ActionEvent event) {
+        if (checkBoxblurEffect.isSelected()){
+            spinnerHigth.setDisable(false);
+            spinnerIteration.setDisable(false);
+            spinnerWidth.setDisable(false);
+            BoxBlur bb = new BoxBlur();
+            bb.setWidth(spinnerWidth.getValue());
+            bb.setHeight(spinnerHigth.getValue());
+            bb.setIterations(spinnerIteration.getValue());
+            imagetoModified.setEffect(bb);
+        }
+        else {
+            spinnerHigth.setDisable(true);
+            spinnerIteration.setDisable(true);
+            spinnerWidth.setDisable(true);
+            BoxBlur bb = new BoxBlur();
+            bb.setWidth(0);
+            bb.setHeight(0);
+            bb.setIterations(0);
+            imagetoModified.setEffect(bb);
+        }
 
     }
 
     @FXML
-    void setTextAngleWtihSliderValue(ActionEvent event) {
+     void blurOnImage (MouseEvent event) {
+        BoxBlur bb = new BoxBlur();
+        bb.setWidth(spinnerWidth.getValue());
+        bb.setHeight(spinnerHigth.getValue());
+        bb.setIterations(spinnerIteration.getValue());
+        imagetoModified.setEffect(bb);
 
     }
 
+
     @FXML
-    void setTextTransparenceWtihSliderValue(ActionEvent event) {
+     void resetAllValues (ActionEvent event) {
+        checkBoxblurEffect.setSelected(false);
+        if (!checkBoxblurEffect.isSelected()){
+            spinnerHigth.setDisable(true);
+            spinnerIteration.setDisable(true);
+            spinnerWidth.setDisable(true);
+        }
+        sliderAngle.setValue(0);
+        sliderTransparence.setValue(0);
+        BoxBlur bb = new BoxBlur();
+        bb.setWidth(0);
+        bb.setHeight(0);
+        bb.setIterations(0);
+        imagetoModified.setEffect(bb);
+
+
+    }
+    @FXML
+     void quitApp (ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Avertissement");
+        alert.setHeaderText("Quitter");
+        alert.setContentText("Voulez-vous Quitter le programme?");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            Platform.exit();
+            System.exit(0);
+        }
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imagetoModified.setImage(new Image("/ressource/Javafx_logo_color.png"));
+        imagetoModified.setPreserveRatio(true);
+        imagetoModified.setFitHeight(200);
+
+        sliderAngle.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            textAngle.setText(String.valueOf((int)sliderAngle.getValue()));
+            imagetoModified.setRotate(sliderAngle.getValue());
+        }));
+        sliderTransparence.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            textTransparence.setText(String.valueOf((int)sliderTransparence.getValue()));
+            imagetoModified.setOpacity((100-sliderTransparence.getValue())/100);
+        }
+        ));
+
 
     }
 
